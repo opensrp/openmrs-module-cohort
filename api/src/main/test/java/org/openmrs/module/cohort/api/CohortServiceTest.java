25d71ff6bc581102898ef24c9503a1a334d36cb4
Stephen S. Musoke <ssmusoke@gmail.com>
2018-05-21 08:47:57 +0300

    Dummy Test case for build pipeline

---
+++ api/src/main/test/java/org/openmrs/module/cohort/api/CohortServiceTest.java
@@ -0,0 +1,17 @@
+package java.org.openmrs.module.cohort.api;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Test;
+import org.openmrs.test.BaseModuleContextSensitiveTest;
+
+import static org.junit.Assert.assertTrue;
+
+public class CohortServiceTest extends BaseModuleContextSensitiveTest {
+    protected final Log log = LogFactory.getLog(getClass());
+
+    @Test
+    public void dummyTest() {
+        assertTrue(true);
+    }
+}
